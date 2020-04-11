b58a571d52f8ae962a64ec75b941706eab5421a1
ry <ryanrosello@hotmail.com>
2020-04-11 11:36:25 +1000

    some clean up

--- main.js
+++ main.js
@@ -9,19 +9,6 @@ require('electron-reload')(__dirname, {
 })
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-  win.hide()
-
   let poller = new Poller(5000);
   let failCount = 0;
 
@@ -33,13 +20,15 @@ function createWindow() {
           let jsonResp = JSON.parse(body)
           const contextMenu = Menu.buildFromTemplate([
             {
-              label: '' + build.tooltip,
+              label: '' + build.msg,
+              icon: `./resources/${build.icon}`,
               click: function () {                
                 childProcess.exec(`start chrome --kiosk https://google.com/search?q=${ jsonResp.build_info.build_number }`);
               }
             },
             {
               label: 'Exit',
+              icon: './resources/exit.png',
               click: function () {
                 app.quit()
               }
@@ -60,6 +49,7 @@ function createWindow() {
             },
             {
               label: 'Exit',
+              icon: './resources/exit.png',
               click: function () {
                 app.quit()
               }
@@ -82,77 +72,41 @@ function createWindow() {
   poller.poll();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 
-// Quit when all windows are closed.
+
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
 let tray = null
 app.on('ready', () => {
-  tray = new Tray('./resources/icons8-final-state-40.png')
-  var build;
-
-  request('http://localhost:3000/status?project=ekk&pipeline=big', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      build = getToolTip(body)
-      const contextMenu = Menu.buildFromTemplate([
-        {
-          label: '' + build,
-          click: function () {
-            dialog.showMessageBoxSync(BrowserWindow, {
-              title: 'yo',
-              message: 'dis'
-            })
-          }
-        },
-        {
-          label: 'Exit',
-          click: function () {
-            app.quit()
-          }
-        }
-      ])
-      tray.setToolTip('' + build)
-      tray.setContextMenu(contextMenu)
-    }
-  })
-
+  tray = new Tray('./resources/icons8-final-state-40.png') 
 })
 
 function getToolTip(data) {
   let buildInfo, icon = ''
   buildData = JSON.parse(data)
+  msg = `${ buildData.build_info.build_number } 5 minutes ago`
 
-  if (buildData.build_info.status == 'running') {
-    buildInfo = `ᕕ(ᐛ)ᕗ ${ buildData.build_info.build_number } 5 minutes ago`
+  if (buildData.build_info.status == 'running') {    
+    buildInfo = `ᕕ(ᐛ)ᕗ ${ msg }`
     icon ='icons8-final-state-40.png'
   } else if (buildData.build_info.status == 'failed') {
-    buildInfo = `❌ ${ buildData.build_info.build_number } 5 minutes ago`
+    buildInfo = `❌ ${ msg }`
     icon ='fail.png'
   } else {
-    buildInfo = `✅ ${ buildData.build_info.build_number } 5 minutes ago`
+    buildInfo = `✅ ${ msg }`
     icon ='pass.png'
   }
 
-  return { tooltip: buildInfo, icon: icon }
+  return { tooltip: buildInfo, icon: icon, msg: msg }
 }
