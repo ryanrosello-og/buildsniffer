7e7afa34dcbeeb708dda2be4a5c8a9504e37e743
ry <ryanrosello@hotmail.com>
2020-04-11 11:16:40 +1000

    handle -ve cases

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, Menu, Tray, dialog } = require('electron')
 const Poller = require('./poller')
 const path = require('path')
 const request = require('request');
+var childProcess = require('child_process');
 
 require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules/.bin/electron.cmd')
@@ -20,37 +21,60 @@ function createWindow() {
   // and load the index.html of the app.
   win.loadFile('index.html')
   win.hide()
- 
-  let poller = new Poller(5000);   
+
+  let poller = new Poller(5000);
+  let failCount = 0;
 
   poller.onPoll(() => {
-    request('http://localhost:3000/status?project=ekk&pipeline=big', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        build = getToolTip(body)
-        const contextMenu = Menu.buildFromTemplate([
-          {
-            label: '' + build.tooltip,
-            click: function () {
-              dialog.showMessageBoxSync(BrowserWindow, {
-                title: 'yo',
-                message: 'dis'
-              })
+    if(failCount < 10) {
+      request('http://localhost:3000/status?project=ekk&pipeline=big', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          build = getToolTip(body)
+          let jsonResp = JSON.parse(body)
+          const contextMenu = Menu.buildFromTemplate([
+            {
+              label: '' + build.tooltip,
+              click: function () {                
+                childProcess.exec(`start chrome --kiosk https://google.com/search?q=${ jsonResp.build_info.build_number }`);
+              }
+            },
+            {
+              label: 'Exit',
+              click: function () {
+                app.quit()
+              }
             }
-          },
-          {
-            label: 'Exit',
-            click: function () {
-              app.quit()
+          ])
+          tray.setToolTip('' + build.tooltip)
+          tray.setContextMenu(contextMenu)
+          tray.setImage(`./resources/${build.icon}`)
+          console.log('updated tray => ', build)
+        } else {
+          const contextMenu = Menu.buildFromTemplate([
+            {
+              label: 'Show error(s)',
+              click: function () {
+                console.log('error => ', error)              
+                dialog.showErrorBox('Failed to retrieve build information', `${error.message} \n ${error.stack}`)
+              }
+            },
+            {
+              label: 'Exit',
+              click: function () {
+                app.quit()
+              }
             }
-          }
-        ])
-        tray.setToolTip('' + build.tooltip)
-        tray.setContextMenu(contextMenu)
-        tray.setImage(`./${build.icon}`)
-        console.log('updated tray => ', build)
-      }
-    })
-    
+          ])
+          tray.setToolTip('Something bad happened...')
+          tray.setContextMenu(contextMenu)
+          tray.setImage(`./resources/problem.png`)        
+          failCount++
+        }
+      })
+    } else {
+      console.log('Too many failures, stopped trying')
+    }
+
     poller.poll(); // Go for the next poll        
   });
 
@@ -85,7 +109,7 @@ app.on('activate', () => {
 
 let tray = null
 app.on('ready', () => {
-  tray = new Tray('./icons8-final-state-40.png')
+  tray = new Tray('./resources/icons8-final-state-40.png')
   var build;
 
   request('http://localhost:3000/status?project=ekk&pipeline=big', function (error, response, body) {
@@ -120,13 +144,13 @@ function getToolTip(data) {
   buildData = JSON.parse(data)
 
   if (buildData.build_info.status == 'running') {
-    buildInfo = `ᕕ( ᐛ )ᕗ ${buildData.build_info.build_number} 5 minutes ago`
+    buildInfo = `ᕕ(ᐛ)ᕗ ${ buildData.build_info.build_number } 5 minutes ago`
     icon ='icons8-final-state-40.png'
   } else if (buildData.build_info.status == 'failed') {
-    buildInfo = `❌ ${buildData.build_info.build_number} 5 minutes ago`
+    buildInfo = `❌ ${ buildData.build_info.build_number } 5 minutes ago`
     icon ='fail.png'
   } else {
-    buildInfo = `✅ ${buildData.build_info.build_number} 5 minutes ago`
+    buildInfo = `✅ ${ buildData.build_info.build_number } 5 minutes ago`
     icon ='pass.png'
   }
 
