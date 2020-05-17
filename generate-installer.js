a8c73805ff7dd03561498b8b6275cca64337d8a3
ryan.rosello <ryanrosello@hotmail.com>
2020-05-17 13:24:40 +1000

    updated installer

--- generate-installer.js
+++ generate-installer.js
@@ -1,13 +1,17 @@
 const electronInstaller = require('electron-winstaller');
 
 try {
-    electronInstaller.createWindowsInstaller({
-      appDirectory: './release-builds/yuarpp-win32-x64',
-      outputDirectory: './tmp/build/installer64',
-      authors: 'lIGHTly Toasted.',
-      exe: 'yuarpp.exe'
-    });
-    console.log('It worked!');
-  } catch (e) {
-    console.log(`No dice: ${e.message}`);
-  }
+  let result = electronInstaller.createWindowsInstaller({
+    appDirectory: './release-builds/yuarpp-win32-x64',
+    outputDirectory: './tmp/build/installer64',
+    authors: 'lIGHTly Toasted.',
+    exe: 'yuarpp.exe'
+  });
+  result.then(() => {
+    console.log('Installer generated');
+  }).catch((e) => {
+    console.log(`Failed to create the installer: ${e}`);
+  })
+} catch (e) {
+  console.log(`No dice: ${e.message}`);
+}
